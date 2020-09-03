6242a49708a9ad4d78efbeff34f057ce7bccef2b
kylemacleay <kfm5670@psu.edu>
2020-09-03 20:54:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+unit = input("Enter unit F/f or C/c: ")
+if unit == "F" or unit == "f":
+  print("Converting Fahrenheit to Celsius.")
+
+if unit == "C" or unit == "c":
+  print("Converting Celsius to Fahrenheit")  
+else:
+  print(f"Invalid unit({unit}).")
